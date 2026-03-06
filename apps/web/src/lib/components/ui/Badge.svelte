c76d5b21016758c5de9f76049e6c76a131564832
Marek Jóźwiak <168720167+66HEX@users.noreply.github.com>
2026-03-06 19:50:56 +0100

    style: add keyframe animation to badge component

--- apps/web/src/lib/components/ui/Badge.svelte
+++ apps/web/src/lib/components/ui/Badge.svelte
@@ -1,11 +1,11 @@
 <script>
+	import Shimmer from './Shimmer.svelte';
+
 	let { children } = $props();
 </script>
 
 <div class="inset-shadow rounded-full border border-border bg-background-inset p-1 text-sm">
-	<div
-		class="rounded-full border border-border bg-background px-2.5 py-0.5 text-sm text-foreground-muted"
-	>
-		{@render children()}
+	<div class="rounded-full border border-border bg-background px-2.5 py-0.5 text-sm">
+		<Shimmer>{@render children()}</Shimmer>
 	</div>
 </div>
